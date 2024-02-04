1c330b77e1616659a69af998737c46a3ef433a01
Salma Salim <145558993+salmasalim1404@users.noreply.github.com>
2024-02-04 20:12:36 +0200

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+// import { Details } from "./details.js";
+import { Home } from "./home.module.js";
+// import { Ui } from "./ui.js";
+
+new Home();
+// const det = new Details();
+// const display = new Ui();
